97e2317619f09dd82685784d034e90aa7237392c
Shuvam <shanta.mitra.sm@gmail.com>
2022-11-25 23:04:11 +0530

    added day night ghapla

---
+++ script.js
@@ -0,0 +1,14 @@
+var date = new Date();
+var hours = date.getHours();
+var r = document.querySelector(':root');
+var newformat = hours >= 12 ? 'PM' : 'AM'; 
+
+if (newformat == 'PM') {
+    changeTheme();
+}
+
+function changeTheme() {
+    r.style.setProperty('--one', 'rgb(20, 20, 20)');
+    r.style.setProperty('--two', 'rgb(240, 240, 240)');
+    r.style.setProperty('--three', 'rgb(170, 170, 170)');
+}
